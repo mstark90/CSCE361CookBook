6e8e85b42067cafb41de212c5ae56a1810c53269
jake <jake@jakoLaptop>
2015-03-16 15:28:09 -0500

    #COOK-8:Code for IngredientDAO

--- src/main/java/edu/unl/csce361/group4/cookbook/dao/impl/IngredientDAOImpl.java
+++ src/main/java/edu/unl/csce361/group4/cookbook/dao/impl/IngredientDAOImpl.java
@@ -6,10 +6,18 @@
 package edu.unl.csce361.group4.cookbook.dao.impl;
 
 import edu.unl.csce361.group4.cookbook.Ingredient;
+import edu.unl.csce361.group4.cookbook.MeasuringUnits;
 import edu.unl.csce361.group4.cookbook.dao.IngredientDAO;
+
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -25,43 +33,111 @@ public class IngredientDAOImpl implements IngredientDAO {
         this.dataSource = new JdbcTemplate(dataSource);
     }
 
-    @Override
-    public List<Ingredient> findIngredient(String name, long offset, long count) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+    public List<Ingredient> findIngredient(String name, long offset, long count) 
+    {
+    	String sql = "SELECT * FROM Ingredients WHERE Name = ?";
+    	
+		List<Ingredient> ingredients = dataSource.query(sql, new Object[]{name}, new BeanPropertyRowMapper(Ingredient.class));
+    	
+    	return ingredients;
     }
 
     @Override
-    public void create(Ingredient ingredient) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void create(Ingredient ingredient) 
+    {
+        String sql = "INSERT INTO Ingredient "
+        		+ "(ingredientName, measuringUnits, retailPrice, servingSize, containerAmount) "
+        		+ "VALUES (?, ?, ?, ?, ?)";
+        
+        dataSource.update(sql, new Object[]
+        		{
+        			ingredient.getIngredientName(), 
+        			ingredient.getMeasuringUnits(),
+        			ingredient.getRetailPrice(),
+        			ingredient.getServingSize(),
+        			ingredient.getContainerAmount()
+        		});
     }
 
     @Override
-    public void create(List<Ingredient> ingredients) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void create(List<Ingredient> ingredients) 
+    {
+        for (Ingredient ingr : ingredients)
+        {
+        	this.create(ingr);
+        }
     }
 
     @Override
-    public void modify(Ingredient ingredient) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void modify(Ingredient ingredient) 
+    {
+    	String sql = "UPDATE Ingredient SET ingredientName = ?, measuringUnits = ?, "
+    			+ "retailPrice = ?, servingSize = ?, containerAmount = ? "
+    			+ "WHERE ingredientId = ?";
+    	
+    	dataSource.update(sql, new Object[]
+    		{
+    			ingredient.getIngredientName(),
+    			ingredient.getMeasuringUnits(),
+    			ingredient.getRetailPrice(),
+    			ingredient.getServingSize(),
+    			ingredient.getContainerAmount(),
+    			
+    			ingredient.getIngredientId()
+    		});
     }
 
     @Override
-    public void modify(List<Ingredient> ingredients) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void modify(List<Ingredient> ingredients) 
+    {
+    	for (Ingredient ingr : ingredients)
+    	{
+    		this.modify(ingr);
+    	}
     }
 
     @Override
-    public void delete(Ingredient ingredient) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(Ingredient ingredient) 
+    {
+    	String sql = "DELETE FROM Ingredient WHERE `ingredientId` = ?";
+    	
+    	dataSource.update(sql, 
+    		new Object[] 
+    		{
+    			ingredient.getIngredientId()
+    		});
     }
 
     @Override
-    public void delete(List<Ingredient> ingredients) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(List<Ingredient> ingredients) 
+    {
+    	for (Ingredient ingr : ingredients)
+    	{
+    		this.delete(ingr);
+    	}
     }
 
-    @Override
-    public List<Ingredient> getIngredients(List<Long> ingredientIds) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+    public List<Ingredient> getIngredients(List<Long> ingredientIds) 
+    {
+    	List<Ingredient> ingredients = new ArrayList<Ingredient>();
+    	
+    	for (Long id : ingredientIds)
+    	{
+    		String sql = "SELECT * FROM Ingredient WHERE `ingredientId` = ?";
+    		
+    		ingredients.add(
+    			(Ingredient) dataSource.query(sql, 
+    			new Object[]
+    			{
+    				id
+    			},
+    			new BeanPropertyRowMapper(Ingredient.class)).get(0));
+    	}
+    	
+    	return ingredients;
     }
 }
