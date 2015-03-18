a1d7e64148038784b68383ae97028c5a04426507
jako77 <jako77@gmail.com>
2015-03-17 22:03:10 -0500

    #COOK-10: Corrected SQL queries in UserDAO

--- src/main/java/edu/unl/csce361/group4/cookbook/dao/impl/UserDAOImpl.java
+++ src/main/java/edu/unl/csce361/group4/cookbook/dao/impl/UserDAOImpl.java
@@ -31,28 +31,33 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public User login(String userName, String password) 
     {
-        String sql = "SELECT * FROM User WHERE userName = ? and password = ?";
+        String sql = "SELECT * FROM users WHERE user_name = ? and password = ?";
         
-        return (User)dataSource.query(sql, new Object[]{}, new BeanPropertyRowMapper(Recipe.class)).get(0);
+        return (User)dataSource.query(sql,
+        		new Object[]
+        		{
+        			userName,
+        			password
+        		},
+        		new BeanPropertyRowMapper(Recipe.class)).get(0);
     }
 
     @Override
     public void create(User user) 
     {
-    	String sql = "INSERT INTO User (userName, fullName, oAuthSecret, oAuthToken) VALUES (?, ?, ?, ?)";
+    	String sql = "INSERT INTO users (user_name, password, full_name) VALUES (?, ?, ?)";
     	
     	dataSource.update(sql, 
     			new Object[]
     			{
     				user.getUserName(),
+    				user.getPassword(), //??
     				user.getFullName(),
-    				user.getOauthSecret(),
-    				user.getOauthToken()    				
     			});
     	
     	for (Recipe favs : user.getFavoriteRecipes())
     	{
-    		sql = "INSERT INTO FavoriteRecipeJoin (userId, recipeId) VALUES (?, ?)";
+    		sql = "INSERT INTO favorite_recipes (user_id, recipe_id) VALUES (?, ?)";
     		
     		dataSource.update(sql, 
     				new Object[]
@@ -66,19 +71,88 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public void modify(User user)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    	//Update favorite_recipes
+    	String sql = "";
+    	
+    	for (Recipe item : user.getFavoriteRecipes())
+    	{
+    		sql = "SELECT favorite_recipe_id FROM favorite_recipes WHERE user_id = ? and recipe_id = ?";
+    		
+    		long favorite_recipe_id = dataSource.queryForObject(sql, 
+    				new Object[]
+    				{
+    					user.getUserId(),
+    					item.getRecipeId()
+    				},
+    				Long.class);
+    		
+    		if (favorite_recipe_id == 0)
+    		{
+    			sql = "INSERT INTO favorite_recipes (user_id, recipe_id) VALUES (?, ?)";
+    			
+    			dataSource.update(sql, 
+    					new Object[]
+    					{
+    						user.getUserId(),
+    						item.getRecipeId()
+    					});
+    		}
+    		else
+    		{
+    			sql = "UPDATE favorite_recipes "
+        				+ "SET user_id = ? "
+        				+ "recipe_id = ? "
+        				+ "WHERE favorite_recipe_id = ?";
+    			
+    			dataSource.update(sql,
+    					new Object[]
+						{
+    						user.getUserId(),
+    						item.getRecipeId(),
+    						favorite_recipe_id
+						});
+    		}
+    	}
+    	
+    	//Update user entry
+    	sql = "UPDATE users "
+    			+ "SET user_name = ? "
+    			+ "password = ? "
+    			+ "full_name = ? "
+    			+ "WHERE user_id = ?";
+    	
+    	dataSource.update(sql,
+    			new Object[]
+    			{
+    				user.getUserName(),
+    				user.getPassword(), //??
+    				user.getFullName(),
+    				user.getUserId()
+    			});
+    	
+    	user.getFavoriteRecipes();
     }
 
     @Override
     public void delete(User user) 
     {
-    	//Delete FavoriteRecipes
-        String sql = "DELETE FROM FavoriteRecipeJoin WHERE userId = ?";
+    	//Delete favorite_recipes
+        String sql = "DELETE FROM favorite_recipes WHERE user_id = ?";
         
         dataSource.update(sql, new Object[]{ user.getUserId() });
         
-        //Delete User
-        sql = "DELETE FROM User WHERE userId = ?";
+        //Delete recipe_owners recipes
+        sql = "DELETE FROM recipe_owners WHERE user_id = ?";
+        
+        dataSource.update(sql, new Object[]{ user.getUserId() });
+        
+        //Delete user's recipes
+        sql = "DELETE FROM recipes WHERE user_id = ?";
+        
+        dataSource.update(sql, new Object[]{ user.getUserId() });
+        
+        //Delete user
+        sql = "DELETE FROM users WHERE user_id = ?";
         
         dataSource.update(sql, new Object[]{ user.getUserId() });
     }
